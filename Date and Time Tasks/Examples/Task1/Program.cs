217a0c68afe7158adb48ec211782d89244532504
icewasovercoded <105386118+icewasovercoded@users.noreply.github.com>
2022-05-26 15:24:27 +0100

    Add files via upload

---
+++ Date and Time Tasks/Examples/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateTime = new DateTime(2022, 4, 28);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            Console.WriteLine("Hour: " + now.Hour);
+            Console.WriteLine("Minutes: " + now.Minute);
+
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+
+            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));
+        }
+    }
+}
---
+++ ImplicitExplicit/ImplicitExplicit/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ImplicitExplicit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1000h6es;
+            byte b = (byte) i;
+            Console.WriteLine(b);
+        }
+    }
+}
